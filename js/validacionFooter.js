484b1944db2c9711708e47b2802eb5087a19ecab
Gabriel Alfredo Boaglio <109044049+Atixar@users.noreply.github.com>
2023-07-26 08:29:07 -0300

    Agrego validacionesal footer

---
+++ js/validacionFooter.js
@@ -0,0 +1,30 @@
+//Validacion para formulario de inscripcion
+
+// Obtener todos los formularios
+const formFooter = document.querySelectorAll(".needs-validation");
+
+// Iterar sobre cada formulario
+Array.from(formFooter).forEach((form) => {
+  // Agregar manejador de envío
+  form.addEventListener("submit", (e) => {
+    // Evitar envío si no es válido
+    if (!form.checkValidity()) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  });
+});
+
+// Validar formulario al hacer clic en botón enviar
+document.getElementById("enviarBtnFooter").addEventListener("click", (e) => {
+  const form = document.getElementById("formularioFooter");
+  if (form.checkValidity()) {
+    // Formulario válido, permitir envío
+  } else {
+    e.preventDefault();
+    e.stopPropagation();
+    form.classList.add("was-validated");
+  }
+});
+
+//Fin Validacion para formulario de inscripcion
